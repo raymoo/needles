f22564ec5fbd5f014ba722d87c5389d91f884d94
Raymoo <uguu@installgentoo.com>
2015-04-17 12:58:48 -0700

    Users of message queues should not be able to take messages from them

--- src/Needles/Bot/Message.hs
+++ src/Needles/Bot/Message.hs
@@ -18,7 +18,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 {-|
 Module      : Needles.Bot.Message
-Description : Generic message queue module
+Description : Generic message queue module. Also PS-specific queue.
 Copyright   : (c) Leon Medvinsky, 2015
 
 License     : GPL-3
@@ -30,21 +30,31 @@ Portability : ghc
 
 module Needles.Bot.Message (
                              mkMessageQueue
+                           , mkPSQueue
                            ) where
 
 import           Control.Concurrent      (ThreadId, forkIO, threadDelay)
-import           Control.Concurrent.Chan (Chan, newChan, readChan)
+import           Control.Concurrent.Chan (Chan, newChan, readChan, writeChan)
 import           Control.Exception
 import           Control.Monad
 import           Data.Text               (Text)
+import qualified Network.WebSockets as WS
 
 -- First argument is what to do with received messages. Second argument is
 -- what to do if there is an error. Third argument is how many microseconds to
 -- wait between processing messages. Returns a Chan to send to.
-mkMessageQueue :: Exception e => (a -> IO ()) -> (e -> IO ()) -> Int -> IO (Chan a, ThreadId)
+mkMessageQueue :: Exception e => (a -> IO ()) -> (e -> IO ()) -> Int
+                  -> IO (a -> IO (), ThreadId)
 mkMessageQueue action handler delay = do
   chan <- newChan
   tId <- forkIO $ catch (messageLoop chan) handler
-  return (chan, tId)
+  return (writeChan chan, tId)
   where messageLoop channel =
           forever $ readChan channel >>= action >> threadDelay delay
+
+-- Creates a queue to send pokemon showdown messages on the provided websocket
+-- connection.
+mkPSQueue :: Exception e => WS.Connection -> (e -> IO ())
+             -> IO (Text -> IO (), ThreadId)
+mkPSQueue conn handler = mkMessageQueue (WS.sendTextData conn) handler delay
+  where delay = 100000 -- PS throttles messages
